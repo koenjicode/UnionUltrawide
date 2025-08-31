2d45e1038e733762219c5ee9a1938e485ff421ff
Jordan Addams <43590463+koenjicode@users.noreply.github.com>
2025-08-31 03:09:07 +0100

    ultrawide menu support

--- Scripts/config.lua
+++ Scripts/config.lua
@@ -2,9 +2,16 @@
 
 local _union_ultrawide_config = {
 
+    -- the FOV used in-game.
+    field_of_view = 116,
+
     -- Aspect Ratio Option
     -- 0 = Y-Axis FOV, 1 = X-Axis FOV, 2 = Major Axis
-    axis_constraint_type = 1,
+    axis_constraint_type = 0,
+
+    -- EXPERIMENTAL: Removes aspect ratio restriction in menus.
+    -- Hud elements may not be designed to fit elements in an ultrawide setting.
+    ultrawide_menu_widgets = true,
 }
 
 return _union_ultrawide_config
