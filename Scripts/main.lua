04c9805a74aa84e897656a03b20b156276da92d4
Jordan Addams <43590463+koenjicode@users.noreply.github.com>
2025-08-31 03:09:46 +0100

    Easier PokeCamera call

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -1,22 +1,54 @@
 local UEHelpers = require("UEHelpers")
 local Config = require "config"
 
-local function PokeCamera()
-    local PlayerController = UEHelpers.GetPlayerController()
-    local context = PlayerController:GetClass():GetFullName()
+local isRaceMode = false
 
-    if context == "BlueprintGeneratedClass /Game/01_Union/Blueprint/System/Input/BP_RacePlayerController.BP_RacePlayerController_C" then
-        local pawn = PlayerController.K2_GetPawn()
-        local raceCamera = pawn.TPCameraComponent
+function PokeCamera(cameraToPoke)
+    cameraToPoke.AspectRatioAxisConstraint = Config.axis_constraint_type
+    cameraToPoke.bConstrainAspectRatio = false
 
-        print(string.format("[UnionUltrawide] Poking race camera at %s...\n", raceCamera:GetFullName()))
-        raceCamera:SetAspectRatioAxisConstraint(Config.axis_constraint_type)
-        raceCamera:SetConstraintAspectRatio(false)
-    end
+    print(string.format("[UnionUltrawide] Poked %s.\n", cameraToPoke:GetFullName()))
 end
 
 RegisterHook("/Script/Engine.PlayerController:ClientRestart", function()
 
     print("[UnionUltrawide] Load level detected, attempting to poke camera...")
-    PokeCamera()
+    local PlayerController = UEHelpers.GetPlayerController()
+    local context = PlayerController:GetClass():GetFullName()
+
+    if context == "BlueprintGeneratedClass /Game/01_Union/Blueprint/System/Input/BP_RacePlayerController.BP_RacePlayerController_C" then
+
+        isRaceMode = true
+
+        local pawn = PlayerController.K2_GetPawn()
+        local raceCamera = pawn.TPCameraComponent
+
+        if raceCamera:IsValid() then
+            PokeCamera(raceCamera)
+        end
+
+        print(string.format("[UnionUltrawide] Adjusting fov to %s...\n", Config.field_of_view))
+        local command = "fov " .. tostring(Config.field_of_view)
+
+        KismetSystemLibrary = StaticFindObject("/Script/Engine.Default__KismetSystemLibrary")
+        KismetSystemLibrary:ExecuteConsoleCommand(UEHelpers.GetWorld(), command, PlayerController)
+    else
+        isRaceMode = false
+    end
+end)
+
+NotifyOnNewObject("/Script/UNION.RaceCameraActor", function(self)
+    ExecuteInGameThread(function()
+        print(string.format("[UnionUltrawide] RaceCameraActor %s was created, poking...\n", self:GetFullName()))
+        PokeCamera(self.RaceCameraComponent)
+    end)
+end)
+
+NotifyOnNewObject("/Script/CinematicCamera.CineCameraActor", function(self)
+    ExecuteInGameThread(function()
+        if Config.ultrawide_menu_widgets and not isRaceMode then
+            print(string.format("[UnionUltrawide] CineCameraActor %s was created, poking...\n", self:GetFullName()))
+            PokeCamera(self.CameraComponent)
+        end
+    end)
 end)
